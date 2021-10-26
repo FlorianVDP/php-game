19a7d4fe52f6156003f83d873cb79743251d4003
Florian <mrvdpflorian@gmail.com>
2021-10-26 15:56:22 +0200

    jeu pokemon fonctionel

--- pokemon.php
+++ pokemon.php
@@ -1,79 +1,136 @@
 <?php
-$salameche = [
-    'nom' => "\e[0;31;40mSalameche",
-    'vie' => (int)10,
-    'element' => 'feu',
-    'attaques' => [
-        'flamèche' => (int)2,
-        'bisoux magique' => (int)1.5,
-    ]
+$pokemons = [
+    $salameche = [
+        'nom' => "\e[0;30;41mSalameche\e[0;37;0m",
+        'vie' => 39,
+        'element' => 'feu',
+        'vitesse' => 65,
+        'actions' => [
+            'attaque' => 12,
+            'regeneration' => 5,
+        ]
+    ],
+    $bulbizarre = [
+        'nom' => "\e[0;30;42mBulbizarre\e[0;37;0m",
+        'vie' => 45,
+        'element' => 'plante',
+        'vitesse' => 60,
+        'actions' => [
+            'attaque' => 49,
+            'regeneration' => 7,
+        ]
+    ],
+    $carapuce = [
+        'nom' => "\e[0;30;44mCarapuce\e[0;37;0m",
+        'vie' => 44,
+        'element' => 'eau',
+        'vitesse' => 63,
+        'actions' => [
+            'attaque' => 48,
+            'regeneration' => 5,
+        ]
+    ],
+    $pikachu = [
+        'nom' => "\e[0;30;43mPikachu\e[0;37;0m",
+        'vie' => 40,
+        'element' => 'electrique',
+        'vitesse' => 70,
+        'actions' => [
+            'attaque' => 12,
+            'regeneration' => 6,
+        ]
+    ],
+    $evoli = [
+        'nom' => "\e[0;30;47mEvoli\e[0;37;0m",
+        'vie' => 50,
+        'element' => 'normal',
+        'vitesse' => 65,
+        'actions' => [
+            'attaque' => 10,
+            'regeneration' => 5,
+        ]
+    ],
 ];
-$bulbizarre = [
-    'nom' => "\e[0;32;40mBulbizarre",
-    'vie' => (int)10,
-    'element' => 'plante',
-    'attaques' => [
-        'liane' => (int)2,
-        'liqueur' => (int)1.5,
-    ]
-];
-$carapuce = [
-    'nom' => "\e[0;34;40mCarapuce",
-    'vie' => (int)10,
-    'element' => 'eau',
-    'attaques' => [
-        'bulle do' => (int)2,
-        'aqua-bisous' => (int)1.5,
-    ]
-];
-$pokemons = [$salameche,$bulbizarre,$carapuce];
+$question = "Qui choisissez vous ? ";
+foreach ($pokemons as $key => $pokemon) {
+    $question .= $pokemon["nom"] . "($key) ";
+}
 print_r("Le combat commence !\n");
-$choixJoueur = (int)readline("Qui choisissez vous ? ".$pokemons[0]['nom']."[0]\e[1;37;40m, ".$pokemons[1]['nom']."[1] \e[1;37;40mou ".$pokemons[2]['nom']."[2]\e[1;37;40m ?");
+$choixJoueur = (int)readline($question);
 $choixJoueur = $pokemons[$choixJoueur];
-$choixPc = $pokemons[rand(0,2)];
-print_r("\e[1;37;40mVottre adversaire à choisi: ".$choixPc['nom']."\n");
-if ($choixJoueur['element'] === 'eau' && $choixPc['element'] === 'plante'){
-    $choixPc['attaques'][0] = 3;
+$choixPc = $pokemons[rand(0, count($pokemons) - 1)];
+print_r("\n");
+print_r("Vottre adversaire à choisi: " . $choixPc['nom'] . "\n");
+print_r("\n");
+
+
+//-----_____----- Ajustement type
+if ($choixJoueur['element'] === 'eau' && $choixPc['element'] === 'plante') {
+    $choixPc['actions']['attaque'] = $choixPc['actions']['attaque'] * 1.5; // faire x1,5
 }
-if ($choixJoueur['element'] === 'plante' && $choixPc['element'] === 'feu'){
-    $choixPc['attaques'][0] = 3;
+if ($choixJoueur['element'] === 'plante' && $choixPc['element'] === 'feu') {
+    $choixPc['actions']['attaque'] = $choixPc['actions']['attaque'] * 1.5;
 }
-if ($choixJoueur['element'] === 'feu' && $choixPc['element'] === 'eau'){
-    $choixPc['attaques'][0] = 3;
+if ($choixJoueur['element'] === 'feu' && $choixPc['element'] === 'eau') {
+    $choixPc['actions']['attaque'] = $choixPc['actions']['attaque'] * 1.5;
 }
-if ($choixJoueur['element'] === 'feu' && $choixPc['element'] === 'plante'){
-    $choixJoueur['attaques'][0] = 3;
+if ($choixJoueur['element'] === 'feu' && $choixPc['element'] === 'plante') {
+    $choixJoueur['actions']['attaque'] = $choixJoueur['actions']['attaque'] * 1.5;
 }
-if ($choixJoueur['element'] === 'plante' && $choixPc['element'] === 'eau'){
-    $choixJoueur['attaques'][0] = 3;
+if ($choixJoueur['element'] === 'plante' && $choixPc['element'] === 'eau') {
+    $choixJoueur['actions']['attaque'] = $choixJoueur['actions']['attaque'] * 1.5;
 }
-if ($choixJoueur['element'] === 'eau' && $choixPc['element'] === 'feu'){
-    $choixJoueur['attaques'][0] = 3;
+if ($choixJoueur['element'] === 'eau' && $choixPc['element'] === 'feu') {
+    $choixJoueur['actions']['attaque'] = $choixJoueur['actions']['attaque'] * 1.5;
 }
+
+
 $compteur = 1;
 
-while(!$choixJoueur <= 0 && !$choixPc <=0){
-    print_r("\e[1;37;40mTour numéro°".$compteur. " ---------------------------------------------\n");
+$vieMaxJoueur = $choixJoueur['vie'];
+$vieMaxPc = $choixPc['vie'];
+
+while ($choixJoueur['vie'] > 0 && $choixPc['vie'] > 0) {
+
+    print_r("Tour numéro°" . $compteur . " ---------------------------------------------\n");
     print_r("\n");
-    $choixAttaqueJoueur = (int)readline("[\e[1;37;40m".$choixJoueur['nom']." \e[1;37;40m|\e[1;37;40m "."".$choixJoueur['vie']."pv] Quelle attaque choisissez vous ? (attaque[0] | santé[1])");
-    if ($choixAttaqueJoueur === 0){
-        $choixPc['vie'] = $choixPc['vie'] - $choixJoueur['attaques'][0];
-        print_r("\e[1;37;40mLe pokemon de votre adversaire [".$choixPc['nom']."\e[1;37;40m à subit -".$choixJoueur['attaques'][0]." ]\n");
-        print_r("\e[1;37;40mIl lui reste ".$choixPc['vie']."pv \n");
-    }else{
-        $choixJoueur['vie'] = $choixJoueur['vie'] + $choixJoueur['attaques'][0];
-        print_r($choixJoueur['nom']." \e[1;37;40mregagne ".$choixJoueur['attaque'][1]."pv\n");
+    print_r("[" . $choixPc['nom'] . " \e[0;31;40m" . $choixPc['vie'] . "pv\e[0;37;0m]\n");
+    //-----_____----- Attaque joueur
+    $choixAttaqueJoueur = (int)readline("[" . $choixJoueur['nom'] . " \e[0;31;40m" . $choixJoueur['vie'] . "pv\e[0;37;0m] Quelle attaque choisissez vous ? (attaque[0] | santé[1])");
+    if ($choixAttaqueJoueur === 0) {
+        $choixPc['vie'] = $choixPc['vie'] - $choixJoueur['actions']['attaque'];
+        print_r("-- Le pokemon de votre adversaire à subit [-" . $choixJoueur['actions']['attaque'] . "pv], il lui reste [" . $choixPc['vie'] . "pv] \n");
+    } else {
+        if ($choixJoueur['vie'] + $choixJoueur['action']['regeneration'] < $vieMaxJoueur) {
+            $choixJoueur['vie'] = $choixJoueur['vie'] + $choixJoueur['actions']['attaque'];
+            print_r("-- Votre pokemon regagne " . $choixJoueur['attaque']['regeneration'] . "pv\n");
+        } else {
+            print_r("-- Votre pokemon à déja tous ses pv\n");
+        }
     }
-    $choixAttaquePc = rand(0,1);
-    if ($choixAttaquePc === 0){
-        $choixJoueur['vie'] = $choixJoueur['vie'] - $choixPc['attaques'][0];
-        print_r("\e[1;37;40mVotre pokemon [".$choixJoueur['nom']." \e[1;37;40mà subit -".$choixPc['attaques'][0]." ]\n");
-        print_r("\e[1;37;40mIl lui reste ".$choixJoueur['vie']."pv \n");
-    }else{
-        $choixPc['vie'] = $choixPc['vie'] + $choixPc['attaques'][0];
-        print_r($choixPc['nom']." \e[1;37;40mregagne ".$choixPc['attaques'][1]."pv\n");
+
+
+    //-----_____----- Attaque pc
+    $choixAttaquePc = rand(0, 10);
+    if ($choixAttaquePc < 5) {
+        $choixJoueur['vie'] = $choixJoueur['vie'] - $choixPc['actions']['attaque'];
+        print_r("-- Votre pokemon à subit -[" . $choixPc['actions']['attaque'] . "pv], il lui reste [" . $choixJoueur['vie'] . "pv] \n");
+    } else {
+
+        if ($choixPc['vie'] + $choixPc['action']['regeneration'] < $vieMaxPc) {
+            $choixPc['vie'] = $choixPc['vie'] + $choixPc['actions']['regeneration'];
+            print_r("-- Le pokemon de votre adversaire regagne [" . $choixPc['actions']['regeneration'] . "pv]\n");
+        } else {
+            print_r("-- Le pokemon à déja tous ses pv\n");
+        }
     }
     print_r("\n");
     $compteur++;
 }
+print_r("---------------------------------------------\n");
+print_r("---------------------------------------------\n");
+print_r("\n");
 print_r('Le combat est terminé !');
+print_r("\n");
+print_r("---------------------------------------------\n");
+print_r("---------------------------------------------\n");
